17ccaae50d8774ad676d4b8d27eee5159b34127d
Daniel Hiltgen <daniel.hiltgen@docker.com>
2017-03-31 17:13:29 -0700

    Migrate machine provisioning logic into e2e repo
    This migrates some of the machine lifecycle management rigging from
    another tree over to the e2e repo.

---
+++ machines/virsh_inject_engine/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/docker-e2e/machines"
+)
+
+// Use this utility to periodically take a base image and create various
+// specialized images with different engines injected for subsequent testing to
+// speed things up so the engine doesn't have to be installed every time The
+// goal is to make it "easy" to build these so we can keep the base patched,
+// then rapidly re-provision the specialized images.
+func main() {
+	log.SetLevel(log.DebugLevel)
+
+	machines, _, err := machines.NewVirshMachines(1, 0)
+
+	if err != nil {
+		log.Fatalf("Failure: %s", err)
+	}
+	m := machines[0].(*machines.VirshMachine)
+
+	// Figure out our exact server version
+	dclient, err := m.GetEngineAPI()
+	if err != nil {
+		log.Fatalf("Failure: %s", err)
+	}
+	ver, err := dclient.ServerVersion(context.Background())
+	if err != nil {
+		log.Fatalf("Failure: %s", err)
+	}
+	versionString := ver.Version
+
+	// When layering the disks it seems SELinux gets angry
+	out, err := m.MachineSSH("sudo touch /.autorelabel")
+	if err != nil {
+		log.Fatalf("Failure: %s: %s", err, out)
+	}
+
+	baseDisk := m.BaseDisk
+	diskFile := m.DiskPath
+	// Immediately delete it, but preserve the disk, then rename the disk
+	err = m.RemoveAndPreserveDisk()
+	if err != nil {
+		log.Fatalf("Failure: %s", err)
+	}
+
+	i := strings.LastIndex(baseDisk, ".")
+	newName := fmt.Sprintf("%s-%s%s", baseDisk[:i], versionString, baseDisk[i:])
+	log.Infof("Renaming %s -> %s", diskFile, newName)
+	os.Rename(diskFile, newName)
+}
